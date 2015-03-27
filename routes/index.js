c9557c4d6e09b87c46639f05438ac62d7dc63de5
unknown <z07042@z07042.h3c.huawei-3com.com>
2015-03-27 17:47:43 +0800

    add template

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Voting' });
+// });
+
+router.get('/', function(req, res, next) { 
+	console.log(process.cwd());
+	res.sendfile('.public/index.html'); 
 });
 
 /* GET user vote page. */
@@ -13,12 +18,12 @@ router.get('/user_vote', function(req, res, next) {
 
 /* POST user vote . */
 router.post('/user_vote', function(req, res, next) {
-	//console.log(req.body.data);
-
+	//console.log(req);
 	res.type('json');
   res.send({
   	ip: req.ip,
   	data: req.body.data,
+  	date: new Date(),
   });
 });
 
