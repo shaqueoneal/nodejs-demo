ac904972d9b767fa109981600fb1d5b03c5a3dd8
root <root@UIS-SERVER2.(none)>
2015-04-09 17:04:47 +0800

    vote ok

---
+++ models.js
@@ -0,0 +1,124 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+// Define User schema
+var _User = new Schema({
+	id: String,
+	name : String,
+	ip : String,
+	email : String,
+	password : String
+});
+
+//one theme one vote record
+var _Vote = new Schema({
+	id: String,
+	userId: String,
+	themeId: String,
+	votes: [{no: String, date: Date, comments: String}],
+});
+
+// theme is something can be voted
+var _Theme = new Schema({
+	id: String,
+	name: String,
+	begin: Date,	// open voting time
+	end: Date,		// close voting time
+	votesPerUser: String, //votes a user can have
+	maxUser: String,	// max users  -1: unlimited
+	maxVotes: String	// max votes  -1: unlimited
+});
+
+var User = mongoose.model('User', _User);
+var Vote = mongoose.model('Vote', _Vote);
+var Theme = mongoose.model('Theme', _Theme);
+
+function add(entity, cb) {
+	if (!entity.id) {
+		console.log("error: invalid entity");
+		return;
+	}
+
+	var self = this;
+
+	this.find({id: entity.id},  function (err, doc) {
+		console.log(doc);
+		if (doc.length <= 0) {
+			self.create(entity, function (err, entity) {
+				if (err) {
+					console.log(err);
+				}
+
+				cb ? cb(err, doc) : undefined;
+			});				
+		}
+		else {
+			console.log("info: entity already exists");
+
+			cb ? cb(err, doc) : undefined;
+		}		
+	});
+}
+
+function del(id, cb) {
+	this.remove({id: id}, function (err, ret) {
+		if (err) {
+			console.log(err);
+		}
+
+		cb ? cb(err, ret) : undefined;
+	});	
+}
+
+function get(id, cb) {
+	this.find({id: id},  function (err, doc) {
+		if (err) {
+			console.log(err);
+		}
+
+		if (doc.length <= 0) {
+			console.log("info: no entity found");
+		}
+
+		cb ? cb(err, doc) : undefined;
+		
+	});
+}
+
+function set(entity, cb) {
+	var self = this;
+	this.find({id: entity.id},  function (err, doc) {
+		if (err) {
+			console.log(err);
+		}
+
+		if (doc.length <= 0) {
+			console.log("info: no entity found");
+			self.add(entity, cb);
+		}
+		else {
+			//update
+			self.del(entity.id, function (err, ret) {
+				console.log(entity);
+				if (err) {
+					console.log(err);
+					cb ? cb(err, entity) : undefined;
+				}
+				else {
+					self.add(entity, cb);
+				}				
+			});	
+		}
+	});
+}
+
+Vote.add = Theme.add = User.add = add;
+Vote.del = Theme.del = User.del = del;
+Vote.get = Theme.get = User.get = get;
+Vote.set = Theme.set = User.set = set;
+
+// export them
+exports.User = User;
+exports.Vote = Vote;
+exports.Theme = Theme;
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,121 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var models = require('../models') // 导入自定义组件 
+
+var User = models.User;  // 使用User模型，对应的users表 
+var Theme = models.Theme;  // 使用Theme模型，对应的Theme表 
+var Vote = models.Vote;  // 使用Vote模型，对应的vote表 
+
+mongoose.connect('mongodb://localhost/user_vote');  // 连接数据库
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Voting' });
 // });
 
-router.get('/', function(req, res, next) { 
+router.get('/', function(req, res, next) {   
 	console.log(process.cwd());
-	res.sendfile('.public/index.html'); 
+
+  var user = {
+    id: req.ip,
+    name : req.ip,
+    ip : req.ip,
+    email : "",
+    password : ""
+  };
+
+  User.add(user, function (err, doc) {
+    if (err) {
+      return;
+    }
+  });
+
+  var theme = {
+    id: 'uis',
+    name: 'UIS首页',
+    begin: Date.now(),  
+    end: new Date(2015, 3, 30), //lasts 10 days 
+    votesPerUser: 1, 
+    maxUser: -1,  
+    maxVotes: -1 
+  };
+
+  Theme.add(theme, function (err, doc) {
+    if (err) {
+      return;
+    }
+  });
+
+  next();	
 });
+
+// router.get('/', function(req, res, next) { 
+//   res.sendFile('.public/index.html'); 
+// });
 
 /* GET user vote page. */
 router.get('/user_vote', function(req, res, next) {
   res.render('user_vote', { title: 'user_vote' });
 });
 
+
 /* POST user vote . */
 router.post('/user_vote', function(req, res, next) {
-	//console.log(req);
-	res.type('json');
-  res.send({
-  	ip: req.ip,
-  	data: req.body.data,
-  	date: new Date(),
-  });
+  //console.log(app);   this will be {}!!
+
+	 console.log(req.body);
+
+ //  var user = new User({
+ //    id: req.ip,
+ //    name : req.ip,
+ //    ip : req.ip,
+ //    email : "",
+ //    password : ""
+ //  });
+
+ //  console.log(user);
+
+ //      user.add();
+
+ //      console.log(req.body);
+
+      // user.vote(req.body.theme, req.body.no, "", function () {
+        // res.type('json');
+        // res.send({
+        //   "theme":    req.body.theme,
+        //   "no":       req.body.no,
+        //   "comments": ""
+        // });          
+      // });   
+
+    var vote = {
+      id: req.ip + req.body.theme,
+      userId: req.ip,
+      themeId: req.body.theme,  
+      votes: [{no: req.body.no, date: new Date(), comments: req.body.comments}],
+    };
+
+
+
+    Vote.set(vote, function (err, doc) {
+      var ret = 'success';
+
+      if (err) {
+        ret = 'fail';
+      }
+
+      res.type('json');
+      res.send({
+        "result":   ret,
+        "theme":    req.body.theme,
+        "no":       req.body.no,
+      }); 
+    });
+
 });
+
+
+ // proxy = new proxy('a’,’b’,’c’,function(a,b,c){ render('index’,…)}) db.findByType(0,function(doc,err){emit('a’,doc)}); db.findByType(1,function(doc,err){…); db.findByType(2,function(doc,err){…);
 
 module.exports = router;
